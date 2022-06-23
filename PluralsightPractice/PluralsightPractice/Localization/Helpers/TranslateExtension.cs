9f96e9ed8abe2d3bf6fce13615526f133f924681
juniorsaraviao <junior.saravia.o@uni.pe>
2022-06-22 22:48:01 -0500

    Add xlf files

---
+++ PluralsightPractice/PluralsightPractice/Localization/Helpers/TranslateExtension.cs
@@ -0,0 +1,21 @@
+﻿using PluralsightPractice.Localization.Resources;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PluralsightPractice.Localization.Helpers
+{
+   [ContentProperty("Text")]
+   public class TranslateExtension : IMarkupExtension
+   {
+      public string Text { get; set; }
+
+      public object ProvideValue(IServiceProvider serviceProvider)
+      {
+         if (Text == null)
+            return null;
+
+         return AppResource.ResourceManager.GetString(Text, AppResource.Culture);
+      }
+   }
+}
