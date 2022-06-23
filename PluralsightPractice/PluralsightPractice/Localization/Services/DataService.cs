6a97a547b3308a5d94f597b5f9fa41ec68f3ce0c
juniorsaraviao <junior.saravia.o@uni.pe>
2022-06-22 22:24:31 -0500

    Add json and Viewmodel

---
+++ PluralsightPractice/PluralsightPractice/Localization/Services/DataService.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using PluralsightPractice.Localization.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluralsightPractice.Localization.Services
+{
+   public class DataService
+   {
+      private readonly string ResourcePath = typeof(DataService).Namespace;
+      private Assembly ThisAssembly = typeof(DataService).GetTypeInfo().Assembly;
+
+      public async Task<ExchangeRate> GetExchangeRate()
+      {
+         string jsonData = "";
+
+         var stream = ThisAssembly.GetManifestResourceStream($"{ResourcePath}.exchange.json");
+
+         using (var reader = new StreamReader(stream))
+         {
+            jsonData = await reader.ReadToEndAsync();
+         }
+
+         var exchangeRates = ExchangeRate.FromJson(jsonData);
+
+         return exchangeRates;
+      }
+
+      public async Task<ExchangeRate> GetExchangeRate(HttpClient client)
+      {
+         var jsonData = await client.GetStringAsync("https://openexchangerates.org/api/latest.json?app_id=YOUR_API_KEY");
+
+         var exchangeRates = ExchangeRate.FromJson(jsonData);
+
+         return exchangeRates;
+      }
+
+      private static string GetClosestLanguage(string locale)
+      {
+         var langs = new[] { "es", "de", "zh-cn", "zh-tw", "pt-br", "pt-pt" };
+         var result = "";
+
+         foreach (var lang in langs)
+         {
+            if (locale.StartsWith(lang, StringComparison.CurrentCultureIgnoreCase))
+            {
+               result = $"{lang}";
+               break;
+            }
+         }
+
+         return result;
+      }
+
+      public List<CurrencyName> GetExchangeCurrencies(string locale)
+      {
+         var language = GetClosestLanguage(locale);
+         var json = "";
+
+         var stream = ThisAssembly.GetManifestResourceStream($"{ResourcePath}.currencies-{language}.json") ??
+                      ThisAssembly.GetManifestResourceStream($"{ResourcePath}.currencies.json");
+
+         using (var reader = new StreamReader(stream))
+         {
+            json = reader.ReadToEnd();
+         }
+
+         return JsonConvert.DeserializeObject<List<CurrencyName>>(json);
+      }
+
+      public async Task<List<CurrencyName>> GetExchangeCurrencies(HttpClient client)
+      {
+         var json = await client.GetStringAsync("https://openexchangerates.org/api/currencies.json");
+
+         return JsonConvert.DeserializeObject<List<CurrencyName>>(json);
+      }
+
+      public List<CurrencySymbol> GetCurrencySymbols()
+      {
+         var json = "";
+
+         var stream = ThisAssembly.GetManifestResourceStream($"{ResourcePath}.currencysymbols.json");
+
+         using (var reader = new StreamReader(stream))
+         {
+            json = reader.ReadToEnd();
+         }
+
+         return JsonConvert.DeserializeObject<List<CurrencySymbol>>(json);
+      }
+   }
+}
---
+++ PluralsightPractice/PluralsightPractice/Localization/ViewModels/CurrencyExchangeViewModel.cs
@@ -0,0 +1,162 @@
+﻿using PluralsightPractice.Localization.Models;
+using PluralsightPractice.Localization.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PluralsightPractice.Localization.ViewModels
+{
+   class CurrencyExchangeViewModel : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+
+      private int _SelectedCurrencyIndex;
+      public int SelectedCurrencyIndex
+      {
+         get { return _SelectedCurrencyIndex; }
+         set
+         {
+            if (_SelectedCurrencyIndex != value)
+            {
+               _SelectedCurrencyIndex = value;
+               OnPropertyChanged();
+            }
+         }
+      }
+
+      private double _curValue = 10.0;
+
+      public double CurValue
+      {
+         set
+         {
+            if (_curValue != value)
+            {
+               _curValue = value;
+               OnPropertyChanged();
+            }
+         }
+         get { return _curValue; }
+      }
+
+      private CurrencyName _selectedExchangeCurrency;
+      public CurrencyName SelectedCurrency
+      {
+         get { return _selectedExchangeCurrency; }
+         set
+         {
+            if (_selectedExchangeCurrency != value)
+            {
+               _selectedExchangeCurrency = value;
+               OnPropertyChanged();
+               Recalc();
+            }
+         }
+      }
+
+      private List<CurrencyName> _CurList;
+      public List<CurrencyName> CurList
+      {
+         get { return _CurList; }
+         set
+         {
+            if (_CurList != value)
+            {
+               _CurList = value;
+               OnPropertyChanged();
+            }
+         }
+      }
+
+      public ICommand RecalcCommand { get; private set; }
+
+      public ObservableCollection<ExchangeData> CurrencyData { get; } = new ObservableCollection<ExchangeData>();
+
+      public ExchangeRate ExchangeRate { get; private set; }
+
+      private DataService dataService;
+
+      public CurrencyExchangeViewModel()
+      {
+         _SelectedCurrencyIndex = -1;
+         dataService = new DataService();
+
+         var currentLocale = System.Threading.Thread.CurrentThread.CurrentUICulture;
+
+         CurList = dataService.GetExchangeCurrencies(currentLocale.Name).OrderBy(s => s.Name).ToList();
+
+         RecalcCommand = new Command(async () => await RecalcAsync(), canExecute: () => { return true; });
+      }
+
+      public async Task InitData()
+      {
+         ExchangeRate = await dataService.GetExchangeRate();
+
+         List<ExchangeData> data = new List<ExchangeData>();
+
+         List<CurrencySymbol> symbols = dataService.GetCurrencySymbols();
+
+         foreach (var r in ExchangeRate.Rates)
+         {
+            var newData = new ExchangeData
+            {
+               CurrencyCode = r.Key,
+               CurrencySymbol = symbols.FirstOrDefault(s => s.Code.Equals(r.Key)).Symbol,
+               CurrencyName = CurList.FirstOrDefault(s => s.Code.Equals(r.Key)).Name,
+               UsdRate = r.Value
+            };
+
+            data.Add(newData);
+         }
+         // Return the exchange rates in country order
+         data = data.OrderBy(d => d.CurrencyName).ToList();
+
+         // slight performance hit, but quick enough for the demo and allows for refreshing of data
+         CurrencyData.Clear();
+         foreach (var d in data)
+         {
+            CurrencyData.Add(d);
+         }
+
+         // Set the picker to US Dollar
+         SelectedCurrencyIndex = _CurList.FindIndex(s => s.Code.Equals("USD"));
+      }
+
+      async Task RecalcAsync()
+      {
+         await Task.Run(() => Recalc());
+
+      }
+      public void Recalc()
+      {
+         double thisRate = 1.0;
+
+         if (_selectedExchangeCurrency != null)
+         {
+            var rate = CurrencyData.FirstOrDefault(c => c.CurrencyCode.Equals(_selectedExchangeCurrency.Code, StringComparison.CurrentCultureIgnoreCase));
+
+            if (rate != null)
+            {
+               thisRate = rate.UsdRate;
+            }
+         }
+
+         foreach (var ce in CurrencyData)
+         {
+            ce.CurrencyRate = ce.UsdRate / thisRate;
+            ce.CurrencyValue = _curValue * ce.CurrencyRate;
+         }
+      }
+   }
+}
