9fbe246e01a52f0795446304dc3c024c111147b8
juniorsaraviao <junior.saravia.o@uni.pe>
2022-07-27 22:54:08 -0500

    Add effect with parameters

---
+++ PluralsightPractice/PluralsightPractice.Android/ShadowEffect.cs
@@ -0,0 +1,37 @@
+﻿using PluralsightPractice.Droid;
+using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(ShadowEffect), nameof(ShadowEffect))]
+namespace PluralsightPractice.Droid
+{
+   public class ShadowEffect : PlatformEffect
+   {
+      protected override void OnAttached()
+      {
+         try
+         {
+            var control = Control as Android.Widget.TextView;
+            var effect = (Effects.ShadowEffect)Element.Effects.FirstOrDefault(e => e is Effects.ShadowEffect);
+            if (effect != null)
+            {
+               float radius = effect.Radius;
+               float distanceX = effect.DistanceX;
+               float distanceY = effect.DistanceY;
+               Android.Graphics.Color color = effect.Color.ToAndroid();
+               control.SetShadowLayer(radius, distanceX, distanceY, color);
+            }
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
+         }
+      }
+
+      protected override void OnDetached()
+      {
+      }
+   }
+}
---
+++ PluralsightPractice/PluralsightPractice/Effects/ShadowEffect.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace PluralsightPractice.Effects
+{
+   public class ShadowEffect : RoutingEffect
+   {
+      public float Radius { get; set; }
+      public Color Color { get; set; }
+      public float DistanceX { get; set; }
+      public float DistanceY { get; set; }
+      public ShadowEffect() : base($"Pluralsight.{nameof(ShadowEffect)}")
+      {
+      }
+   }
+}
